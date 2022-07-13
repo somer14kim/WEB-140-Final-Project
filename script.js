f5a85f75133b28e63a99d785086c1e9e3387794c
derrickjs <85529367+derrickjs@users.noreply.github.com>
2022-07-13 14:29:18 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+
+$('.foot').click(function(){
+    alert("All rights reserved. Content is the intellectual property of Group B inc. Please contact for any questions about using content.")
+});
+  
+});
